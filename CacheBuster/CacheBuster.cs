25f2c3e714688ba169b6f8b5d1223b037ef797db
Simon Nørlund Eldevig <sne@novicell.dk>
2020-09-08 21:06:51 +0200

    Add CacheBuster and test project

---
+++ CacheBuster/CacheBuster.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Web;
+using System.Web.Caching;
+using System.Web.Hosting;
+
+namespace CacheBuster
+{
+    public static class CacheBuster
+    {
+        public static string GetFullPath(string rootRelativePath)
+        {
+            if (HttpRuntime.Cache[rootRelativePath] == null)
+            {
+                string absolute = HostingEnvironment.MapPath("~" + rootRelativePath);
+
+                DateTime date = File.GetLastWriteTime(absolute);
+
+                string result = $"{rootRelativePath}?v={date.Ticks}";
+                HttpRuntime.Cache.Insert(rootRelativePath, result, new CacheDependency(absolute));
+            }
+
+            return HttpRuntime.Cache[rootRelativePath] as string;
+        }
+    }
+}
